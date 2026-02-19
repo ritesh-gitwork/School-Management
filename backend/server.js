38a504a28ec9c6f002fa702c69d3ef378b6ed204
Ritesh Gupta <riteshmca.321@gmail.com>
2026-02-19 13:07:07 +0530

    cors error solved

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,22 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://school-management-alpha-khaki.vercel.app"
+];
+
 app.use(cors({
-  origin: ["http://localhost:5173",
-    "https://school-management-alpha-khaki.vercel.app/"
-  ],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 }));
+
 
 
 app.use(express.json());
